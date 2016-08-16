6e635523a95763728468984062026fc0e10ea6e9
Bill Zissimopoulos <billziss@navimatics.com>
2016-08-16 11:18:50 -0700

    initial commit

---
+++ src/sys/driver.c
@@ -0,0 +1,18 @@
+#include <devioctl.h>
+#include <ntifs.h>
+#include <ntstrsafe.h>
+#include <wdmsec.h>
+
+DRIVER_INITIALIZE DriverEntry;
+
+#ifdef ALLOC_PRAGMA
+#pragma alloc_text(INIT, DriverEntry)
+#endif
+
+NTSTATUS DriverEntry(
+    PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
+{
+    (VOID)DriverObject;
+    (VOID)RegistryPath;
+    return STATUS_NOT_IMPLEMENTED;
+}
