42b7911236d98b4712e66647204cd3dc345acaf4
Ilya Finkelshteyn <ilyaf@appveyor.com>
2019-08-02 12:48:02 -0700

    Try use lastexitcode

--- build.ps1
+++ build.ps1
@@ -3,3 +3,4 @@ $env:CONFIGURATION = "Release"
 appveyor AddMessage "Reboot complete" -Category Information
 bcdedit | findstr /i "testsigning"
 tools\build.bat $env:CONFIGURATION
+if ($LastExitCode -ne 0) {$host.SetShouldExit($LastExitCode)}
