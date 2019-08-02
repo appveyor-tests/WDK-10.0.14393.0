31cf12217fe0c92b218c70d7027dfe145be4f4cb
Ilya Finkelshteyn <ilyaf@appveyor.com>
2019-08-02 12:55:18 -0700

    Uncomment

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 [xml]$targets = get-content "C:\Program Files (x86)\Windows Kits\10\build\WindowsDriver.Common.targets"
 $usingTask = $targets.ChildNodes[1].UsingTask | ? {$_.TaskName -eq "ValidateNTTargetVersion"}
 $usingTask.AssemblyFile = '$(WDKContentRoot)build\bin\Microsoft.DriverKit.Build.Tasks.16.0.dll'
-#$targets.Save("C:\Program Files (x86)\Windows Kits\10\build\WindowsDriver.Common.targets")
+$targets.Save("C:\Program Files (x86)\Windows Kits\10\build\WindowsDriver.Common.targets")
 git submodule update --init --recursive
 appveyor AddMessage "Change boot configuration and reboot" -Category Information
 bcdedit /set testsigning on
